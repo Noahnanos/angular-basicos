85cae889786876455617d2fb18d90474e7f997d7
Max Alexander Jara Fierro <noahnanos@gmail.com>
2022-01-14 12:00:39 -0300

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component,  } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.scss']
+})
+export class ListadoComponent  {
+
+    heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor','Captain America'];
+    heroeBorrado: string = '';
+    
+    
+    borrarHeroe() {
+      this.heroeBorrado  = this.heroes.shift() || '';
+      
+    }
+
+}
